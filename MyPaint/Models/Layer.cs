cbcf6a578cbd2822630c0818109a28c580b03f05
lolitamasalstudy@gmail.com <lolitamasalstudy@gmail.com>
2026-04-02 11:26:26 +0300

    Layer.cs

--- MyPaint/Models/Layer.cs
+++ MyPaint/Models/Layer.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using MyPaint.Models.Shapes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyPaint.Models
 {
-    internal class Layer
+    public class Layer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsVisible { get; set; }
+        public bool IsLocked { get; set; }
+        public List<Shape> Shapes { get; set; }
+
+        public Layer() 
+        {
+            Id = Id;
+            Name = Name;
+            IsVisible = true;
+            IsLocked = false;
+            Shapes = new List<Shape>();
+        }
+
+        
     }
 }
